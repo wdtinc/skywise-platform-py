9e57ca1402084b9e5dc5744151cb8afa80485316
Justin Stewart <jstewart@wdtinc.com>
2016-09-20 11:17:59 -0500

    Added changelist. Tweaked travis.yml.

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name='skywise-platform',
-    version='1.0.6',
+    version='0.0.1',
     package_data={'': ['README.md']},
     packages=['skywiseplatform'],
     install_requires=[
