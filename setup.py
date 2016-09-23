ff20db809fa2abf6407190534c75b5c041fba54d
Justin Stewart <jstewart@wdtinc.com>
2016-09-23 10:03:33 -0500

    Incrementing version.

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name='skywise-platform',
-    version='0.0.1',
+    version='0.1.0',
     package_data={'': ['README.md']},
     packages=['skywiseplatform'],
     install_requires=[
